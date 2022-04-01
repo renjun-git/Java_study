543527e437ca134229f3bf9fa9585937a14e42e8
renjun <renjun@163.com>
2022-04-01 16:27:26 +0800

    Initial commit

---
+++ src/main/java/If_Else.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.nio.CharBuffer;
+import java.util.Scanner;
+
+/**
+ * @Author rj
+ * @Date 2022/4/1 14:44
+ * @Version 1.0
+ */
+public class If_Else {
+/**
+ * if_else 语法案例
+ *
+ * if(){
+ *
+ * }
+ * elseif(){
+ *
+ * }
+ * elseif(){
+ *
+ * }
+ * else{
+ *
+ * }
+ */
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int age = scanner.nextInt();
+    if (age>18){
+        System.out.println("你已经"+age+"了");
+    }else if(age == 10){
+        System.out.println("====");
+    }
+    else {
+        System.out.println("你太小啦");
+    }
+
+
+    /**
+     * 练习
+     *
+     * &&就是and  ||就是or
+     */
+    double x=20;
+    double y=40;
+    if (x>10 && y<50){
+        System.out.println(x+y);
+
+    }
+}
+}
---
+++ src/main/java/Switch.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+/**
+ * @Author rj
+ * @Date 2022/4/1 15:39
+ * @Version 1.0
+ */
+public class Switch {
+    /**
+     * Switch语法
+     *
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        switch (s){
+            case "a":
+                System.out.println("1");
+                break;
+            case "b":
+                System.out.println("2");
+                break;
+            case "c":
+                System.out.println("3");
+                break;
+            default:
+                System.out.println("111");
+                break;
+
+        }
+    }
+}
